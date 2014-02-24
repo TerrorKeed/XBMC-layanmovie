743dcd30b696aee5fe1d5aad7eaa6614ba6004a9
durdle <ddurdle@gmail.com>
2014-02-24 00:10:03 -0500

    new file:   LICENSE 	modified:   README.md 	new file:   addon.xml 	new file:   changelog.txt 	new file:   copyFromDebug.sh 	new file:   default.py 	new file:   icon.png 	new file:   resources/language/english/strings.xml 	new file:   resources/settings.xml

---
+++ copyFromDebug.sh
@@ -0,0 +1,4 @@
+cp ~/.xbmc/addons/gdrive/default.py /u01/Dropbox/development/xbmc/XBMC-gdrive/
+cp ~/.xbmc/addons/gdrive/resources/lib/gdrive.py /u01/Dropbox/development/xbmc/XBMC-gdrive/resources/lib/
+cp ~/.xbmc/addons/gdrive/resources/language/english/strings.xml /u01/Dropbox/development/xbmc/XBMC-gdrive/resources/language/english/
+cp ~/.xbmc/addons/gdrive/resources/settings.xml /u01/Dropbox/development/xbmc/XBMC-gdrive/resources/
